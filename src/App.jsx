fd8eacaa12262ce96ff344b28903cf7fc87e6bd5
jaswanth <jaswanth@gmail.com>
2026-04-08 20:54:25 +0530

    react basic install done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hello React!</h1>
+      <p>This is a basic React app with a beautiful background.</p>
+    </div>
+  )
+}
+
+export default App
